aa5bb35d579091d3318f7757711a7fbf2fc4e8e2
ComradeCosmos <dhyeymendpara@gmail.com>
2025-03-19 20:36:52 +0530

    Update 6.py

--- 4/6.py
+++ 4/6.py
@@ -1,9 +1,12 @@
-for hour in range(24):
-    if hour == 0:
-        print("12:00 AM") 
-    elif hour == 12:
-        print("12:00 PM")  
-    elif hour < 12:
-        print(str(hour) + ":00 AM")
-    else:
-        print(str(hour - 12) + ":00 PM")
+time = int(input("Enter time in 24-hour format: "))
+
+if time < 12 and time > 0:
+    print(time,"AM")
+elif time == 12:
+    print("Noon")
+elif time > 12 and time < 24:
+    print(time-12,"PM")
+elif time == 24 or time == 0:
+    print("Midnight")
+else:
+    print("Invalid time")
